7fcdc6cdc04f69da256150922cee25165e1fbdf1
chrislam2 <97582406+chrislam2@users.noreply.github.com>
2025-05-26 09:26:19 +0800

    Delete Install R Library.R

--- Install R Library.R
+++
@@ -1,9 +0,0 @@
-load.lib<-c("devtools","Rcpp","xml2","XML","plyr","dplyr","tidyverse","vroom","lifecycle","lifecycle","lubridate","stringr","openxlsx","readr","rvest",
-            "filesstrings","httr","xlsReadWrite")
-
-install.lib <- load.lib[!load.lib %in% installed.packages()]
-
-for(lib in install.lib) 
-    install.packages(lib, dependencies=TRUE, method = "wininet")
-                        
-sapply(load.lib,require,character=TRUE)
